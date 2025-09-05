cfb2cb32d61d99ad3ecf87c6aba58bddc3fd1c0a
Arman Jasuja <arman_jasuja@yahoo.com>
2025-09-06 00:38:46 +0400

    Started on list structure, defined map

--- deep_revis/3A_lecture/3A.scm
+++ deep_revis/3A_lecture/3A.scm
@@ -35,3 +35,30 @@
 (-vec v1 v2) ;; -> (-2, -4)
 (scale 2 v1) ;; -> (4 . 8)
 (scale 2 (+vec v1 v2)) ;; -> (12 . 24)
+
+(define make-segment cons)
+(define seg-start car)
+(define seg-end cdr)
+
+;; Define the map function
+(define (_map f a)
+  (if (eq? a '())
+      a
+      (cons (f (car a)) (_map f (cdr a)))))
+
+(define l (list 1 2 3 4))
+
+(define (square x)
+  (* x x))
+
+(_map square l) ;; -> 1 4 9 16
+
+
+;; Construct the generic idea of repeating a function n times
+(define (repeated f n)
+  (lambda(x)
+    (if (= n 0)
+	x
+	(f ((repeated f (-1+ n)) x)))))
+
+((repeated square 2) 3) ;; -> 81
